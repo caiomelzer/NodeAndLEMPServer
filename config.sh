9f6b17199b51d5fb5a3b5e183bdfebd207339563
Caio Melzer <melzer.caio@gmail.com>
2017-04-17 14:16:34 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,13 +6,13 @@ echo "*********************************************************"
 echo ""
 echo "Step 1: Configuration of Domain caiomelzer.ga"
 mkdir /usr/share/nginx/html/caiomelzer.ga
-cp caiomelzer.ga /etc/nginx/sites-avaliable/
-ln -s /etc/nginx/sites-avaliable/caiomelzer.ga /etc/nginx/sites-enabled/caiomelzer.ga
+cp caiomelzer.ga /etc/nginx/sites-available/
+ln -s /etc/nginx/sites-available/caiomelzer.ga /etc/nginx/sites-enabled/caiomelzer.ga
 
 echo "Step 2: Configuration of Domain myweb.tk"
 mkdir /usr/share/nginx/html/myweb.tk
-cp myweb.tk /etc/nginx/sites-avaliable/
-ln -s /etc/nginx/sites-avaliable/myweb.tk /etc/nginx/sites-enabled/myweb.tk
+cp myweb.tk /etc/nginx/sites-available/
+ln -s /etc/nginx/sites-available/myweb.tk /etc/nginx/sites-enabled/myweb.tk
 
 echo "Step 3: Restarting Server"
 sudo service nginx restart
